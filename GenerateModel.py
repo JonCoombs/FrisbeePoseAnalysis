3d54c13b56294deda86186cb1d5615f19a544b1a
JonCoombs <jonathan.d.coombs@gmail.com>
2024-08-25 13:22:10 -0400

    Initial commit
    GenerateModel.py is fully set up and tested, though a larger dataset is needed to train a better model.

---
+++ GenerateModel.py
@@ -0,0 +1,97 @@
+from ultralytics import YOLO
+from PIL import Image
+from os import listdir
+
+#Begin modifiable global variables
+dataset = "datasets/Frisbee Form Analysis.v4i.yolov8"   #Change path to match current dataset
+
+training = False        #Are you training a new model?
+numEpochs = 300         #Number of "epochs", or individual instances of training on a subset of the dataset. Generally from 100 to 500 for this project
+batchSize = 16          #Number of images to train on per epoch. Generally between 12 and 20 for this project; my GPU ran out of memory while training on 20
+patienceVal = 100       #Number of epochs w/o improvement before stopping training early. 100 is a good baseline, and you should change it based on numEpochs and your computer
+
+visualizeModel = True  #Are you visualizing an existing model?
+modelNum = 0            #0 if visualizing the most recent model, otherwise visualizes the chosen model number.
+                        #This variable does nothing if visualizeModel is False.
+#End modifiable global variables
+
+
+def train():
+    model = YOLO("yolov8n-pose.pt")
+
+    #Generate a model
+    model.train(
+        data = dataset + "/data.yaml",
+        imgsz = 1280,   #Default, can change if the images are pre-processed differently
+        epochs=numEpochs,
+        batch=batchSize,
+        device="cuda",  #Switch to "cpu" if you don"t have both a GPU and some form of CUDA installed. CPU takes much longer per epoch, and is not recommended
+        patience=patienceVal
+    )
+
+def visualize(modelNum):
+    #Select the best training weights from the correct model. If modelNum = 1, select "train" instead of "train1"
+    model = YOLO("runs/pose/train" + ("" if modelNum == 1 else str(modelNum)) + "/weights/best.pt")
+
+    #Run the model on all images in the most recent test folder
+    test_folder = dataset + "/test/images"
+
+    testing_count = 1
+
+    for item in listdir(test_folder):
+
+        #Ignore other files. Currently, all images will always be .jpg files
+        if not item.endswith(".jpg"):
+            print("Not a test image.")
+            continue
+    
+        annotated = model(test_folder + "/" + item)
+
+        #Use Pillow to plot annotations to images
+        r = annotated[0]
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+
+        #Save the annotated images to a folder for later review
+        im.save("annotations/temp" + str(testing_count) + ".jpg")
+
+        testing_count += 1
+
+#Prevent other branches from running training (when used with GPU)
+if __name__ == "__main__" and training:
+    train()
+
+#Test out the trained model
+if visualizeModel:
+    #Select the most recent model if modelNum is 0
+    if modelNum == 0:
+        for item in listdir("runs/pose"):
+            trainNum = item[5:]
+            trainNum = (1 if trainNum == "" else int(trainNum))
+            if trainNum > modelNum:
+                modelNum = trainNum
+    
+    #Call visualize on the correct model
+    visualize(modelNum)
+    
+# model = YOLO("runs/pose/train25/weights/best.pt")
+
+# test_folder = "datasets/Frisbee Form Analysis.v4i.yolov8/test/images"
+
+# testimg_count = 1
+
+# for item in listdir(test_folder):
+#     print(item)
+
+#     if not item.endswith(".jpg"):
+#         print("Not jpg")
+#         continue
+    
+#     annotated = model(test_folder + "/" + item)
+
+#     r = annotated[0]
+#     im_array = r.plot()  # plot a BGR numpy array of predictions
+#     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+#     im.save("annotations/temp" + str(testimg_count) + ".jpg")
+
+#     testimg_count += 1
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FrisbeePoseAnalysis
 Pose estimation and analysis of ultimate frisbee throwing form using Roboflow and YOLOv8 image detection
+
+Currently a WIP, GenerateModel.py uses YOLOv8 on a pose estimation dataset to generate labeling models with the goal of completely automating labeling new videos. Datasets are not included on GitHub, but can be found on Roboflow: https://app.roboflow.com/miscellaneous-projects-1aldw/frisbee-form-analysis-cfr2t
+
+Once the generated models are accurate enough, analysis will be done on the actual movements and timestamps of those movements to analyze various players' throwing forms.
